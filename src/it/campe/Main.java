57a26ec0b1c0a6a9a1f6b81cbb2493906d928f29
JanaZhakPl <janazhakpl@gmail.com>
2021-10-23 19:34:59 +0200

    Done.

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -1,9 +1,17 @@
 package it.campe;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.nextLine();
+        String s2 = scanner.nextLine();
+        String s3 = scanner.nextLine();
+        System.out.println(s3);
+        System.out.println(s2.toUpperCase(Locale.ROOT));
+        System.out.println(s1.toLowerCase(Locale.ROOT));
     }
 }
